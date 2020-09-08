23127162177656484c4b12ab86e7fb40956f6ee6
chalista29 <70938640+chalista29@users.noreply.github.com>
2020-09-08 14:12:03 +0700

    Add files via upload

---
+++ uts1.java
@@ -0,0 +1,11 @@
+package uts;
+
+public class uts1 {
+    public static void main(String[] args) {
+        String huruf="ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+        System.out.println(""+ huruf.charAt(2) + huruf.charAt(7) + huruf.charAt(0) +  huruf.charAt(11) + huruf.charAt(8) + huruf.charAt(18) + huruf.charAt(19) + huruf.charAt(0) + huruf.charAt(27) + huruf.charAt(21) + huruf.charAt(0) + huruf.charAt(13) + huruf.charAt(8) + huruf.charAt(0));
+
+    }
+
+}
